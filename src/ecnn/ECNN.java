37d924d619ba8bc9b8d688737c9bec2a534e0a8b
dhwaj3 <ihc2016003@iiita.ac.in>
2018-01-19 12:48:08 +0530

    New comit

---
+++ src/ecnn/ECNN.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecnn;
+
+/**
+ *
+ * @author Dhwaj
+ */
+public class ECNN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
